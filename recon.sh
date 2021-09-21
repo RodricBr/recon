66efb589c96aef51a8d57ddf0faa4b911827258d
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-09-21 13:59:58 -0300

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -6,6 +6,6 @@ else
   # Pega os .JS e joga num arquivo indicando os que estão ativos
   gau -subs $1 | grep -iE '\.js'| grep -iEv '(\.jsp|\.json)' >> js.txt ; cat js.txt | anti-burl | awk '{print $4}' | sort -u >> $1.Alive.txt
 
-  # Listagem de subdomínios usando anubis
+  # Listagem de subdomínios usando anubis e httpx retornando os status codes
   curl -s https://jldc.me/anubis/subdomains/$1 | httpx -title -tech-detect -status-code >> $1.Subdomains.txt
 fi
