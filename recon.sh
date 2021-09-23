79532ad8378902d69bea921ed3b5dbb3ef4f86af
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-09-23 19:28:44 -0300

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 #!/usr/bin/bash
 
 check_g(){
@@ -33,27 +35,42 @@ if [[ -z "$@" ]] || [[ "$@" == "-h" ]]; then
 else
   echo -e "Executando comandos, aguarde por favor!\n\
   Isso pode levar alguns minutos!"
-  mkdir reconlogs/
+  ARQ=$(mkdir -p reconlogs/)
+  if [ ! -d "$ARQ" ]; then
+    echo "Arquivo existe"
+  fi
   # Pega os .JS e joga num arquivo indicando os que estão ativos
   if [[ "${*: -1}" == "G" ]]; then
     check_g
-    # Pega os .JS e joga num arquivo indicando os que estão ativos
-    gau -subs "$1" | grep -iE '\.js'| grep -iEv '(\.jsp|\.json)' >> reconlogs/"$1"-js.txt ; cat reconlogs/"$1"-js.txt | anti-burl | awk '{print $4}' | sort -u >> reconlogs/"$1"-ativos.txt
-    #if [[  ]]; then
-    #  echo "a"
-    #else
-    #  echo "b"
-    #fi
+    gau_(){
+      # Pega os .JS e joga num arquivo indicando os que estão ativos
+      gau -subs "$1" | grep -iE '\.js'| grep -iEv '(\.jsp|\.json)' >> reconlogs/"$1"-js.txt ; cat reconlogs/"$1"-js.txt | anti-burl | awk '{print $4}' | sort -u >> reconlogs/"$1"-ativos.txt
+      if [ -d "$ARQ" ]; then
+        echo "Movendo logs pro reconlogs/"
+      fi
+    }
+    gau_
     exit 0
   elif [ "${*: -1}" '==' "O" ]; then # *: -1 == último caractere
     check_o
-    # Listagem de subdomínios usando anubis
-    gospider -q -s "https://$1" | awk NF | anew >> reconlogs/"$1"-spider-subs.txt
+    gospider_(){
+      # Listagem de subdomínios usando anubis
+      gospider -q -s "https://$1" | awk NF | anew >> reconlogs/"$1"-spider-subs.txt
+      if [ -d "$ARQ" ]; then
+        echo "Movendo logs pro reconlogs/"
+      fi
+    }
     exit 0
   elif [ "${*: -1}" '==' "F" ]; then
     check_f
-    # Listagem de subdomínios usando findomain-linux
-    findomain-linux --quiet --target "http[s]?:\/\/$1" | anew >> reconlogs/"$1"-findomain-subs.txt #https://"$1" >> reconlogs/"$1"-findomain-subs.txt
+    find_(){
+      # Listagem de subdomínios usando findomain-linux
+      findomain-linux --quiet --target http[s]?://$1 | anew >> reconlogs/"$1"-findomain-subs.txt #https://"$1" >> reconlogs/"$1"-findomain-subs.txt
+      if [ -d "$ARQ" ]; then
+        echo "Movendo logs pro reconlogs/"
+      fi
+    }
+    find_
     exit 0
     #subfinder
   fi
