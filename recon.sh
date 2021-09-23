e23c90a58472aa665c5beac82016a9753d008034
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-09-23 16:16:44 -0300

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -3,12 +3,35 @@
 if [[ -z "$@" ]]; then
   echo -e "Uso: $0 <URL sem http>"
 else
+  if [[ -n $(command -v gau -h) ]]; then
+      echo -e "${VERDE}Gau instalado!${FIM}"
+      echo -e "${VERDE}Versão:${FIM} $(gau --version)"
+      exit 0
+  else
+      echo -e "${VERMELHO}Gau não encontrado!${FIM}"
+      exit 1
+  fi
   echo -e "Executando comandos, aguarde por favor!\n\
   Isso pode levar alguns minutos!"
   # Pega os .JS e joga num arquivo indicando os que estão ativos
-  gau -subs $1 | grep -iE '\.js'| grep -iEv '(\.jsp|\.json)' >> js.txt ; cat js.txt | anti-burl | awk '{print $4}' | sort -u >> Ativos.txt
+  if [[ "$*" == "-G" ]]; then
+    # Pega os .JS e joga num arquivo indicando os que estão ativos
+    gau -subs "$1" | grep -iE '\.js'| grep -iEv '(\.jsp|\.json)' >> "$1"-js.txt ; cat "$1"-js.txt | anti-burl | awk '{print $4}' | sort -u >> "$1"-ativos.txt
+    exit 0
+  elif [ "$*" == "-O" ]; then # *: -1 == último caractere
+    # Listagem de subdomínios usando anubis
+    gospider -q -s "https://$1" | awk NF >> "$1"-subs.txt
+    exit 0
+  elif [ "$*" == "-F" ]; then
+    # Listagem de subdomínios usando findomain-linux
+    findomain-linux --quiet --target https://"$1"
+    exit 0
+  fi
 
-  # Listagem de subdomínios usando anubis
-  gospider -q -s "https://$1" >> Subdomains.txt
+  #if [[ "$?" == 0 ]]; then
+  #  echo "sucesso"
+  #else
+  #  echo "falhou"
+  #fi
   exit 0
 fi
